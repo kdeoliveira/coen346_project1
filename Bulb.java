f2e3300662fe7ee9791b6740d103b956dda5efb2
Kevin de Oliveira <contact@kevindeoliveira.com>
2020-09-29 15:54:48 -0400

    final

--- Bulb.java
+++ Bulb.java
@@ -4,24 +4,59 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+* <h1>Binary tree traversal for defective lightbulbs</h1>
+
+The class {@code Bulb()} is responsible to describe every objet representing a lightbulb as a boolean value. The boolean array is copied from 
+{@link ReadFile#getList() getList()} method. An array for defective bulbs is initialized as a counter of non functionning bulbs. 
+The following methods are executed on the main array {@code bulb[]} and is available to the client:
+<ul>
+    <li>{@code findDefective()}</li>
+    <li>{@code printDefective()}</li>
+    <li>{@code getThreadCounter()}</li>
+</ul>
+<p>
+Internal private methods are responsible for verifying, recursively, the bulb array for any defective lightbulb.
+The search uses the Divide and Conquer algorithm to efficientely check each section of the array for defective bulb.
+The array is divided in two halves at each recursive call if a defective bulb is found. Otherwise the recursion is skipped with a @return void.
+For each half created, a new thread is generated so algorithm can be executed concurrently. To avoid overwritting results, the {@code checkDefective()} and {@code threadCounter} are manipulated atomically.
+</p>
+
+* @author  Kevin de Oliveira
+* @version 1.0
+* @since   2020/07/29
+*/
 
 public class Bulb {
     private boolean[] bulbs;
     private int threadCounter;
     private List<Integer> defective;
     private final int SIZE;
-    
+    /**
+     * Creates an object for a list of bulbs
+     * @param list boolean array
+     */
     public Bulb(boolean[] list){
         this.threadCounter = 0;
         this.SIZE = list.length;
         this.bulbs = Arrays.copyOf(list, SIZE);
         defective = new ArrayList<>();
     }
-
+    /**
+     * Calls recursive search on {@code bulb[]}
+     * @return void
+     */
     public void findDefective(){
         this.findDefective(0, SIZE - 1);
     }
-
+    /**
+     * Recursive search that divides and create new Thread for each section containing defective bulb.
+     * The {@code threadCounter} is incremented via synchronization at each recursion level.
+     * <b> Since threads are created in pairs, the total value of threads created is {@code 2 * threadCounter}</b>
+     * @param low lower index of half section
+     * @param high higher index of half section
+     * @return void
+     */
     private void findDefective(int low, int high){
         if(low >= high || !this.verifyDefective(low, high)){
             this.checkDefective(low);
@@ -29,6 +64,8 @@ public class Bulb {
         }
 
         int mid = (low+high)/2;
+
+        // New thread is created for each section
 
         Thread tLeft = new Thread("left array"){
             @Override
@@ -43,6 +80,7 @@ public class Bulb {
             }
         };
 
+        //Threads start
         tLeft.start();
         tRight.start();
         
@@ -50,7 +88,12 @@ public class Bulb {
             this.threadCounter++; 
         }
     }
-
+    /**
+     * Check if array section contains any defective bulb
+     * @param low lower index of half section
+     * @param high higher index of half section
+     * @return the lightbulb status (defective: true, functionning: false)
+     */
     private boolean verifyDefective(int low, int high){
         boolean flag = false;
 
@@ -60,12 +103,17 @@ public class Bulb {
 
         return flag;
     }
-
+    /**
+     * Adds the index of a defective lightbulb into {@code defecive[]}.
+     * @param item index of defective lightbulb
+     */
     private synchronized void checkDefective(int item){
         if(!bulbs[item])
             defective.add(item+1);
     }
-
+    /**
+     * Prints the results
+     */
     public void printDefective(){
         Logger log = Logger.getLogger("Bulb.class");
 
@@ -75,7 +123,10 @@ public class Bulb {
 
         log.log(Level.INFO, "Number of threads created {0} ", this.threadCounter);
     }
-
+    /**
+     * Get the number of threads generated during search
+     * @return total number of threads created
+     */
     public int getThreadCounter(){
         //Threads executed in pair
         return threadCounter*2;
--- ReadFile.java
+++ ReadFile.java
@@ -1,12 +1,40 @@
 import java.io.*;
+/**
+* <h1>Text file reader</h1>
+
+The class {@code ReadFile()} is responsible to generate a boolean array from an input text file which format respects the the following conditions:
+<ul>
+~/$ text.txt
+<ol>1.  Integer specifying number of lightbulbs</ol>
+<ol>2.  Bulb-1 state (0 or 1)</ol>
+<ol>3.  Bulb-2 state (0 or 1)</ol>
+<ol>4.  Bulb-n state (0 or 1)</ol>
+</ul>
+<p>
+The followign methods are available to the client:
+<ul>
+    <li>{@code getList()}</li>
+</ul>
+<p>
+Once file is generated, a new boolean array will be generated based on the value provided on the input file. The input file can be either specified as an argument on command-line or as a File object.
+</p>
+
+* @author  Kevin de Oliveira
+* @version 1.0
+* @since   2020/07/29
+*/
 public class ReadFile{
     private File src;
-    private final static int EOF = -1;
+    private static final int EOF = -1;
 
     public ReadFile(File file){
         this.src = file;
     }
-
+    /**
+     * Creates new file object
+     * @param args Array of strings
+     * @throws IOException Input file invalid
+     */
     public ReadFile(String[] args) throws IOException{        
         if(args.length > 1)
             throw new IOException("Invalid number of argument");
@@ -15,7 +43,11 @@ public class ReadFile{
                 throw new IOException("Unable to read file");
         
     }
-
+    /**
+     * Generates and returns a boolean array of n size according to @param args[] arguments from command line or @param file file object .
+     * @return boolean array
+     * @throws IOException Input file character or line invalid
+     */
     public boolean[] getList() throws IOException{
 
         boolean[] output;
